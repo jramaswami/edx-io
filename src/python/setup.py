b40a7d821515f050b33e78c0cc346c648d663750
jramaswami <jramaswami@gmail.com>
2017-10-26 15:11:56 -0400

    Added setup.py in order to make installable package.

---
+++ src/python/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'edx_io',
+        version = '0.1',
+        description = 'I/O routines for use in Edx ITMOx: I2CPx',
+        author = 'Maxim Buzdalov')
+
